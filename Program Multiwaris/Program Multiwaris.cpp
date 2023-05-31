5bc63897811bfaf2fbba612281c77bed713f228a
WisnuDwiSeisno <127277412+WisnuDwiSeisno@users.noreply.github.com>
2023-05-31 21:42:38 +0700

    selesai membuat program multiwaris

--- Program Multiwaris/Program Multiwaris.cpp
+++ Program Multiwaris/Program Multiwaris.cpp
@@ -18,4 +18,38 @@ public:
 class manusia {
 public:
 	string jenisKelamin;
+
+	manusia(string pJenisKelamin) :
+		jenisKelamin(pJenisKelamin) {
+		cout << "manusia dibuat\n" << endl;
+	}
+	~manusia(){
+		cout << "manusia dihapus\n" << endl;
+	}
 };
+
+class pelajar : private manusia, public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+		orang(pNama),
+		manusia(pJenisKelamin),
+		sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + "dengan jenis kelamin"
+			+ jenisKelamin + " dari sekolah " + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa("Andi Laksono", "Laki-Laki", "SMAN 1 Bantul");
+	cout << siswa.perkenalan();
+
+	return 0;
+}
